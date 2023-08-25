3909f91d4320e9d4520c11d975a732537b5051fb
max <bitcoinassassination@gmail.com>
2023-08-25 11:03:37 -0400

    adding a datafile class to write and read data later

--- main.cpp
+++ main.cpp
@@ -51,6 +51,57 @@ T prompt_for_numeric(std::string message)
 //    m_fPrice = 4.90
 //  }
 
+class Datafile
+{
+  public:
+
+    inline void set_string(const std::string& sString, std::size_t nItem = 0)
+    {
+      // sets string value of property (for given index, zero by default)
+      if(nItem >= m_vContent.size())
+        m_vContent.resize(nItem + 1);
+      m_vContent[nItem] = sString;
+    }
+
+    inline const std::string get_string(std::size_t nItem = 0) const
+    {
+      // returns string at given index or "" if given index is >= vec size
+      if(nItem >= m_vContent.size())
+        return "";
+      else
+      return m_vContent[nItem];
+    }
+    
+    // == real numbers ==
+    inline void set_real(const float f, const std::size_t nItem = 0)
+    {
+      set_string(std::to_string(f),nItem);
+    }
+
+    inline const float get_real(std::size_t nItem = 0) const
+    {
+      return std::atof(get_string(nItem).c_str());
+    }
+
+    // == integers ==
+    inline void set_int(const int n, const std::size_t nItem = 0)
+    {
+      set_string(std::to_string(n),nItem);
+    }
+
+    inline const int get_int(std::size_t nItem = 0) const
+    {
+      return std::atoi(get_string(nItem).c_str());
+    }
+
+  private:
+    std::vector<std::string> m_vContent {};
+    std::vector<std::pair<std::string, Datafile>> m_vecObjects {};
+    std::unordered_map<std::string, std::size_t> m_mapObjects {};
+};
+
+
+
 class Item
 {
   public:
@@ -88,6 +139,11 @@ class Item
     void set_name(std::string str) {m_sItemName = str;}
     void set_price(int price) {m_fPrice = price;}
     void set_qty(int qty) {m_nQuantity = qty;}
+
+    int get_id() {return m_nId;}
+    std::string get_name() {return m_sItemName;}
+    float get_price() {return m_fPrice;}
+    int get_qty() {return m_nQuantity;}
 
     void update_quantity_add(int qty) {m_nQuantity += qty;}
     void update_quantity_subtract(int qty)
@@ -142,10 +198,9 @@ int main()
 {
   Item i1;
   Container c1;
+  Datafile df;
+
   i1.create_item();
-  i1.print_details();
-  i1.update_name();
-  i1.update_quantity_subtract(prompt_for_numeric<int>(">> Enter quantity to subtract: "));
   i1.print_details();
 
   Item i2;
@@ -154,5 +209,16 @@ int main()
   c1.add_item(i1);
   c1.add_item(i2);
   c1.display_items();
+  
+  df.set_string(i1.get_name());
+  df.set_int(i1.get_id(),1);
+  df.set_int(i1.get_qty(),2);
+  df.set_real(i1.get_price(),3);
+  std::cout << df.get_string();
+  std::cout << df.get_int(1);
+  std::cout << df.get_int(2);
+  std::cout << df.get_real(3);
+
+
   return 0;
 }
